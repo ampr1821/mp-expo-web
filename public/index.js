ab092caa8b3a3a5c9b2bae58cb471095352a8896
Sumedh Hagaldivte <sumedh.hagaldivte@gmail.com>
2023-04-01 11:44:47 +0530

    map zooms into current location of the user

---
+++ public/index.js
@@ -0,0 +1,38 @@
+
+L.Control.geocoder().addTo(map);
+
+if (!navigator.geolocation) {
+    console.log("Your browser doesn't support geolocation feature!");
+} else {
+    navigator.geolocation.getCurrentPosition(getPosition);
+    
+    console.log(" geolocation works");
+}
+
+var marker, circle, lat, long, accuracy;
+
+function getPosition(position) {
+    // console.log(position)
+    lat = position.coords.latitude
+    long = position.coords.longitude
+    accuracy = position.coords.accuracy
+
+    if (marker) {
+        map.removeLayer(marker)
+    }
+
+    if (circle) {
+        map.removeLayer(circle)
+    }
+
+    marker = L.marker([lat, long])
+    circle = L.circle([lat, long], { radius: 400 })
+
+    var featureGroup = L.featureGroup([marker, circle]).addTo(map)
+    
+
+    map.fitBounds(featureGroup.getBounds())
+
+
+    console.log("Your coordinate is: Lat: " + lat + " Long: " + long + " Accuracy: " + accuracy)
+}

28d4d5e7f229b25cbb7e95ccc4b0a7f0ee0dd5af
Amitash M S <amitu895@gmail.com>
2023-04-02 13:24:55 +0530

    Added node js server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+'/public/index.html');
+});
+
+app.listen(3001, () => {
+  console.log('Server listening on port 3000');
+});

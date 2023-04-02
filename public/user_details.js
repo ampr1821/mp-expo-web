4c592f66302a9849d94eeddc69e2d4b4065e35aa
Sumedh Hagaldivte <sumedh.hagaldivte@gmail.com>
2023-04-02 22:28:18 +0530

    added a prompt that takes patient name

---
+++ public/user_details.js
@@ -0,0 +1,40 @@
+
+class CustomPrompt {
+    constructor() {
+        this.render = function (dialog, fctn) {
+            var winW = window.innerWidth;
+            var winH = window.innerHeight;
+            var dialogoverlay = document.getElementById('dialogoverlay');
+            var dialogbox = document.getElementById('dialogbox');
+            dialogoverlay.style.display = "block";
+            dialogoverlay.style.height = winH + "px";
+            dialogbox.style.left = (winW / 2) - (560 * .5) + "px";
+            dialogbox.style.top = "5px";
+            dialogbox.style.display = "block";
+            document.getElementById('dialogboxhead').innerHTML = "A value is required";
+            document.getElementById('dialogboxbody').innerHTML = dialog;
+            document.getElementById('dialogboxbody').innerHTML += '<br><input id="prompt_value1"/>';
+            document.getElementById('dialogboxfoot').innerHTML = '<button onclick="promot.ok(\'' + fctn + '\')">OK</button> <button onclick="promot.cancel()">Cancel</button>';
+        };
+        this.cancel = function () {
+            document.getElementById('dialogoverlay').style.display = "none";
+            document.getElementById('dialogbox').style.display = "none";
+        };
+        this.ok = function (fctn) {
+            var prompt_value1 = document.getElementById('prompt_value1').value;
+            // window[fctn](prompt_value1);
+            console.log("this is the paitent name: "+prompt_value1 )
+            document.getElementById('dialogoverlay').style.display = "none";
+            document.getElementById('dialogbox').style.display = "none";
+        };
+    }
+}
+var promot = new CustomPrompt();
+
+/*Geting Value*/
+function changeText(val){ 
+	document.getElementById('status').innerHTML = val;
+	} 
+function doStuff(val){ 
+	document.body.style.background = val; 
+	} 
